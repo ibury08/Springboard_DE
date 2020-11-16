31c224e868dbf162a58154195134e483d0aca480
Ian Bury <24845508+ibury08@users.noreply.github.com>
2020-11-15 21:35:38 -0800

    initial commit

---
+++ OOP-Banking-System/tests/test_banking_system.py
@@ -0,0 +1,33 @@
+import pytest
+import argparse
+from banking_system import Account, SavingsAccount, CheckingAccount, Customer, withdraw, deposit
+
+
+def test_account():
+    acc = Account(1, 1, 100)
+    with pytest.raises(TypeError):
+        acc.withdraw('d')
+    with pytest.raises(ValueError):
+        acc.withdraw(-10)
+    acc.withdraw(20)
+    assert acc.balance == 80
+    with pytest.raises(ValueError):
+        acc.withdraw(100)
+    with pytest.raises(TypeError):
+        acc.deposit('string')
+    with pytest.raises(ValueError):
+        acc.deposit(-100)
+    acc.deposit(100)
+    assert acc.balance == 180
+
+
+def test_savings():
+    sa = SavingsAccount(1, 1, 1000, 1000)
+    with pytest.raises(ValueError):
+        sa.withdraw(100)
+
+
+def test_checking():
+    ca = CheckingAccount(1, 1, 100)
+    ca.withdraw(101)
+    assert ca.balance == -6
