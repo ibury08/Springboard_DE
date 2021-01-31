23daa62d2e9f2d0d9520dbf35dc4f01f8d2b15be
Ian Bury <24845508+ibury08@users.noreply.github.com>
2021-01-31 10:29:56 -0800

    fixing euro_cup queries

--- euro_cup_mini_project/sql_q17_sol.sql
+++ euro_cup_mini_project/sql_q17_sol.sql
@@ -1,5 +1,5 @@
 -- Write a SQL query to find the country where the most assistant referees come from,
 -- and the count of the assistant referees.
-select country_name, n_refs from 
-(select country_id, count(*) as n_refs from asst_referee_mast group by country_id) A 
-join soccer_country using (country_id);
+select country_name, count(*) as n_refs from asst_referee_mast
+join soccer_country using (country_id)
+group by country_name order by n_refs desc limit 1;
